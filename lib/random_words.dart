7dfa4cd93b5159d608b75e082c5a08dc6cb19b0d
Samuel Adekunle <oluwatomisinadekunle@gmail.com>
2018-10-14 13:17:05 +0100

    Change the type of saved to Set<String>

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -1,10 +1,12 @@
 import "package:flutter/material.dart";
 import "package:english_words/english_words.dart";
+import "package:shared_preferences/shared_preferences.dart";
 
 class RandomWords extends StatefulWidget {
-	RandomWords({ Key key, this.saved }) : super(key: key);
+	RandomWords({ Key key, this.saved, this.prefs }) : super(key: key);
 
-	final Set<WordPair> saved;
+  final SharedPreferences prefs;
+	final Set<String> saved;
 
 	@override
 	_RandomWordsState createState() => _RandomWordsState();
@@ -15,10 +17,11 @@ class _RandomWordsState extends State<RandomWords> {
 	final _biggerFont = TextStyle(fontSize: 18.0);
 
 	Widget _buildListTile(WordPair pair) {
-		final bool _alreadySaved = widget.saved.contains(pair);
+    final String _text = pair.asPascalCase;
+    final bool _alreadySaved = widget.saved.contains(_text);
 		return ListTile(
 			title: Text(
-				pair.asPascalCase,
+        _text,
 				style: _biggerFont,
 			),
 			trailing: Icon(
@@ -28,9 +31,9 @@ class _RandomWordsState extends State<RandomWords> {
 			onTap: () {
 				setState(() {
 					if (_alreadySaved) {
-						widget.saved.remove(pair);
+            widget.saved.remove(_text);
 					} else {
-						widget.saved.add(pair);
+            widget.saved.add(_text);
 					}
 				});
 			},
@@ -39,7 +42,6 @@ class _RandomWordsState extends State<RandomWords> {
 
 	Widget _buildList() {
 		return ListView.builder(
-			padding: EdgeInsets.fromLTRB(0.0, 16.0, 0.0, 16.0),
 			itemBuilder: (context, i) {
 				if (i.isOdd)
 					return Divider();
