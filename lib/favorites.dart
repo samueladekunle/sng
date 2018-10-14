0fb9c04dbba79486283581279d12a4e7c261af4f
Samuel Adekunle <oluwatomisinadekunle@gmail.com>
2018-10-14 08:12:34 +0100

    Create a list for favorites

--- lib/favorites.dart
+++ lib/favorites.dart
@@ -2,20 +2,37 @@ import "package:flutter/material.dart";
 import "package:english_words/english_words.dart";
 
 class Favorites extends StatefulWidget {
-  Favorites({Key key, this.saved}) : super(key: key);
+	Favorites({Key key, this.saved, this.context}) : super(key: key);
 
-  final Set<WordPair> saved;
+	final Set<WordPair> saved;
+	final BuildContext context;
 
-  _FavoritesState createState() => _FavoritesState();
+	@override
+	_FavoritesState createState() => _FavoritesState();
 }
 
 class _FavoritesState extends State<Favorites> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Favorites"),
-      ),
-    );
-  }
+	var tiles, divided;
+	TextStyle _biggerFont = TextStyle(fontSize: 18.0);
+
+	initState() {
+		super.initState();
+		tiles = widget.saved.map((WordPair pair) => ListTile(
+			title: Text(pair.asPascalCase, style: _biggerFont),
+		));
+		divided = ListTile.divideTiles(
+			context: widget.context,
+			tiles: tiles,
+		).toList();
+	}
+
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			appBar: AppBar(
+				title: Text("Favorites"),
+			),
+			body: ListView(children: divided),
+		);
+	}
 }
